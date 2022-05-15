b9f851ff69742f5c2985cd35a860154e2f524b35
Ben Jackson <b@benjackson.email>
2022-05-16 09:29:14 +1000

    test: add development env test to template

--- internal/templating/routes/templates_test.go
+++ internal/templating/routes/templates_test.go
@@ -91,6 +91,40 @@ func TestGenerateKubeTemplate(t *testing.T) {
 			},
 			want: "test-resources/result-custom-ingress1.yaml",
 		},
+		{
+			name: "custom-ingress2",
+			args: args{
+				route: lagoon.RouteV2{
+					Domain:         "extra-long-name.a-really-long-name-that-should-truncate.www.example.com",
+					Service:        "nginx",
+					MonitoringPath: "/",
+					Insecure:       helpers.StrPtr("Redirect"),
+					TLSAcme:        helpers.BoolPtr(true),
+					Migrate:        helpers.BoolPtr(false),
+					Annotations: map[string]string{
+						"custom-annotation": "custom annotation value",
+					},
+					Fastly: lagoon.Fastly{
+						Watch: false,
+					},
+				},
+				values: lagoon.BuildValues{
+					Project:         "example-project",
+					Environment:     "environment-with-really-really-reall-3fdb",
+					EnvironmentType: "development",
+					Namespace:       "myexample-project-environment-with-really-really-reall-3fdb",
+					BuildType:       "branch",
+					LagoonVersion:   "v2.x.x",
+					Kubernetes:      "lagoon.local",
+					Branch:          "environment-with-really-really-reall-3fdb",
+				},
+				monitoringContact:      "abcdefg",
+				monitoringStatusPageID: "12345",
+				monitoringEnabled:      true,
+				activeStandby:          false,
+			},
+			want: "test-resources/result-custom-ingress2.yaml",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
