4a39372061dd9a1cfd8bd441c2e55dbf01b24635
Toby Bellwood <toby.bellwood@amazee.io>
2022-08-18 11:02:59 +1000

    missed a line

--- legacy/build-deploy.sh
+++ legacy/build-deploy.sh
@@ -3,12 +3,11 @@ set -x
 set -eo pipefail
 set -o noglob
 
-set +x # reduce noise in build logs
 # print out the build-deploy-tool version information
 echo "##############################################"
 build-deploy-tool version
 echo "##############################################"
-set -x
+
 
 REGISTRY=$REGISTRY
 NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
