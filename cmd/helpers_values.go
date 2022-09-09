49b623a1723589d48d31bf3cf91e8dab548088da
shreddedbacon <b@benjackson.email>
2022-09-09 11:54:57 +1000

    chore: use helper function for generator input

--- cmd/helpers_values.go
+++ cmd/helpers_values.go
@@ -1,9 +1,37 @@
 package cmd
 
+import generator "github.com/uselagoon/build-deploy-tool/internal/generator"
+
 var lagoonYml, lagoonYmlOverride, environmentName, projectName, activeEnvironment, standbyEnvironment, environmentType string
 var buildType, lagoonVersion, branch, prTitle, prNumber, prHeadBranch, prBaseBranch string
 var projectVariables, environmentVariables, monitoringStatusPageID, monitoringContact string
 var templateValues, savedTemplates, fastlyCacheNoCahce, fastlyServiceID, fastlyAPISecretPrefix string
 var monitoringEnabled bool
 
-var defaultBackupSchedule, hourlyDefaultBackupRetention, dailyDefaultBackupRetention, weeklyDefaultBackupRetention, monthlyDefaultBackupRetention string
+func generatorInput(debug bool) generator.GeneratorInput {
+	return generator.GeneratorInput{
+		Debug:                    debug,
+		LagoonYAML:               lagoonYml,
+		LagoonYAMLOverride:       lagoonYmlOverride,
+		LagoonVersion:            lagoonVersion,
+		ProjectName:              projectName,
+		EnvironmentName:          environmentName,
+		EnvironmentType:          environmentType,
+		ActiveEnvironment:        activeEnvironment,
+		StandbyEnvironment:       standbyEnvironment,
+		ProjectVariables:         projectVariables,
+		EnvironmentVariables:     environmentVariables,
+		BuildType:                buildType,
+		Branch:                   branch,
+		PRNumber:                 prNumber,
+		PRTitle:                  prTitle,
+		PRHeadBranch:             prHeadBranch,
+		PRBaseBranch:             prBaseBranch,
+		MonitoringContact:        monitoringContact,
+		MonitoringStatusPageID:   monitoringStatusPageID,
+		FastlyCacheNoCahce:       fastlyCacheNoCahce,
+		FastlyAPISecretPrefix:    fastlyAPISecretPrefix,
+		IgnoreMissingEnvFiles:    ignoreMissingEnvFiles,
+		IgnoreNonStringKeyErrors: ignoreNonStringKeyErrors,
+	}
+}
