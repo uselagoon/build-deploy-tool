0bc4b5060225adc34d3d28c6941d7d91503eeb1d
shreddedbacon <b@benjackson.email>
2022-09-12 16:12:12 +1000

    chore: disable debug for dbaas identification

--- cmd/identify_dbaas.go
+++ cmd/identify_dbaas.go
@@ -14,7 +14,7 @@ var dbaasIdentify = &cobra.Command{
 	Aliases: []string{"db", "d"},
 	Short:   "Identify if any dbaas consumers are created",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		generator, err := generatorInput(true)
+		generator, err := generatorInput(false)
 		if err != nil {
 			return err
 		}
