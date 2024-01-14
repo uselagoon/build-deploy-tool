3af5722b5198f70fa133fc358860aef1430690e9
Blaize M Kaye <blaize.kaye@gmail.com>
2024-01-15 08:33:37 +1300

    Replaces grype with trivy for cyclonedx generation

--- legacy/scripts/exec-generate-insights-configmap.sh
+++ legacy/scripts/exec-generate-insights-configmap.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 TMP_DIR="${TMP_DIR:-/tmp}"
-SBOM_OUTPUT="cyclonedx-json"
+#SBOM_OUTPUT="cyclonedx-json" -- This is the syft format
+# We're moving to trivy
+SBOM_OUTPUT="cyclonedx"
+
 SBOM_OUTPUT_FILE="${TMP_DIR}/${IMAGE_NAME}.cyclonedx.json.gz"
 SBOM_CONFIGMAP="lagoon-insights-sbom-${IMAGE_NAME}"
 IMAGE_INSPECT_CONFIGMAP="lagoon-insights-image-${IMAGE_NAME}"
@@ -45,7 +48,8 @@ processImageInspect
 echo "Running sbom scan using syft"
 echo "Image being scanned: ${IMAGE_FULL}"
 
-DOCKER_HOST=docker-host.lagoon.svc docker run --rm -v /var/run/docker.sock:/var/run/docker.sock imagecache.amazeeio.cloud/anchore/syft packages ${IMAGE_FULL} --quiet -o ${SBOM_OUTPUT} | gzip > ${SBOM_OUTPUT_FILE}
+#DOCKER_HOST=docker-host.lagoon.svc docker run --rm -v /var/run/docker.sock:/var/run/docker.sock imagecache.amazeeio.cloud/anchore/syft packages ${IMAGE_FULL} --quiet -o ${SBOM_OUTPUT} | gzip > ${SBOM_OUTPUT_FILE}
+DOCKER_HOST=docker-host.lagoon.svc docker run --rm -v /var/run/docker.sock:/var/run/docker.sock imagecache.amazeeio.cloud/aquasec/trivy image ${IMAGE_FULL} --format ${SBOM_OUTPUT} | gzip > ${SBOM_OUTPUT_FILE}
 
 FILESIZE=$(stat -c%s "$SBOM_OUTPUT_FILE")
 echo "Size of ${SBOM_OUTPUT_FILE} = $FILESIZE bytes."
