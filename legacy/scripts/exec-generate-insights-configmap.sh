cf3e46bda8f2c0eaf3150f1ae0e14b00908147ca
Blaize M Kaye <blaize.kaye@gmail.com>
2024-01-23 06:54:22 +1300

    Adds ability to control insights scan image with env vars

--- legacy/scripts/exec-generate-insights-configmap.sh
+++ legacy/scripts/exec-generate-insights-configmap.sh
@@ -8,6 +8,9 @@ SBOM_CONFIGMAP="lagoon-insights-sbom-${IMAGE_NAME}"
 IMAGE_INSPECT_CONFIGMAP="lagoon-insights-image-${IMAGE_NAME}"
 IMAGE_INSPECT_OUTPUT_FILE="${TMP_DIR}/${IMAGE_NAME}.image-inspect.json.gz"
 
+# Here we give the user the ability to override the insights scan image
+INSIGHTS_SCAN_IMAGE="${INSIGHTS_SCAN_IMAGE:-aquasec/trivy}"
+
 set +x
 echo "Running image inspect on: ${IMAGE_FULL}"
 
@@ -45,8 +48,9 @@ processImageInspect
 
 echo "Running sbom scan using trivy"
 echo "Image being scanned: ${IMAGE_FULL}"
+echo "Using image for scan ${IMAGECACHE_REGISTRY}${INSIGHTS_SCAN_IMAGE}"
 
-DOCKER_HOST=docker-host.lagoon.svc docker run --rm -v /var/run/docker.sock:/var/run/docker.sock imagecache.amazeeio.cloud/aquasec/trivy image ${IMAGE_FULL} --format ${SBOM_OUTPUT} | gzip > ${SBOM_OUTPUT_FILE}
+DOCKER_HOST=docker-host.lagoon.svc docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ${IMAGECACHE_REGISTRY}${INSIGHTS_SCAN_IMAGE} image ${IMAGE_FULL} --format ${SBOM_OUTPUT} | gzip > ${SBOM_OUTPUT_FILE}
 
 FILESIZE=$(stat -c%s "$SBOM_OUTPUT_FILE")
 echo "Size of ${SBOM_OUTPUT_FILE} = $FILESIZE bytes."
