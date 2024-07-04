9aa52863854c19a0bb2852b06bb86a614c9109d3
shreddedbacon <b@benjackson.email>
2024-07-05 08:22:36 +1000

    chore: allow docker-host to be configurable

--- legacy/docker-entrypoint.sh
+++ legacy/docker-entrypoint.sh
@@ -4,18 +4,18 @@ set -e
 # try connect to docker-host 10 times before giving up
 DOCKER_HOST_COUNTER=1
 DOCKER_HOST_TIMEOUT=10
-until docker -H docker-host.lagoon.svc info &> /dev/null
+until docker -H ${DOCKER_HOST} info &> /dev/null
 do
 if [ $DOCKER_HOST_COUNTER -lt $DOCKER_HOST_TIMEOUT ]; then
     let DOCKER_HOST_COUNTER=DOCKER_HOST_COUNTER+1
-    echo "docker-host.lagoon.svc not available yet, waiting for 5 secs"
+    echo "${DOCKER_HOST} not available yet, waiting for 5 secs"
     sleep 5
 else
-    echo "could not connect to docker-host.lagoon.svc"
+    echo "could not connect to ${DOCKER_HOST}"
     exit 1
 fi
 done
-export DOCKER_HOST=docker-host.lagoon.svc
+
 
 mkdir -p ~/.ssh
 
