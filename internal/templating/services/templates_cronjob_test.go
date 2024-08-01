9ccad4f430694573e86bb6b5d24b1224812225b3
Tobi Nehrlich <tobi.nehrlich@amazee.io>
2024-08-01 10:41:29 +0200

    chore: Add fsGroupChangePolicy to cron jobs
    Deployments already include the fsGroupChangePolicy OnRootMismatch when
    enabled. Cron jobs that use the same volumes also need this policy set
    to speed up pod startup. This is especially helpful when a volume contains
    a large amount of files.

--- internal/templating/services/templates_cronjob_test.go
+++ internal/templating/services/templates_cronjob_test.go
@@ -81,6 +81,72 @@ func TestGenerateCronjobTemplate(t *testing.T) {
 			},
 			want: "test-resources/cronjob/result-cli-1.yaml",
 		},
+		{
+			name: "test2 - cli - security context",
+			args: args{
+				buildValues: generator.BuildValues{
+					Project:         "example-project",
+					Environment:     "environment-name",
+					EnvironmentType: "production",
+					Namespace:       "myexample-project-environment-name",
+					BuildType:       "branch",
+					LagoonVersion:   "v2.x.x",
+					Kubernetes:      "generator.local",
+					Branch:          "environment-name",
+					ImageReferences: map[string]string{
+						"myservice":         "harbor.example.com/example-project/environment-name/myservice@latest",
+						"myservice-persist": "harbor.example.com/example-project/environment-name/myservice-persistent@latest",
+					},
+					GitSHA:       "0",
+					ConfigMapSha: "32bf1359ac92178c8909f0ef938257b477708aa0d78a5a15ad7c2d7919adf273",
+					PodSecurityContext: generator.PodSecurityContext{
+						RunAsGroup:     0,
+						RunAsUser:      10000,
+						FsGroup:        10001,
+						OnRootMismatch: true,
+					},
+					Services: []generator.ServiceValues{
+						{
+							Name:             "myservice",
+							OverrideName:     "myservice",
+							Type:             "cli",
+							DBaaSEnvironment: "production",
+							NativeCronjobs: []lagoon.Cronjob{
+								{
+									Name:     "cronjob-myservice-my-cronjobbb",
+									Service:  "myservice",
+									Command:  "sleep 300",
+									Schedule: "5 2 * * *",
+								},
+								{
+									Name:     "cronjob-myservice-my-other-cronjobbb",
+									Service:  "myservice",
+									Command:  "env",
+									Schedule: "25 6 * * *",
+								},
+							},
+						},
+						{
+							Name:                 "myservice-persist",
+							OverrideName:         "myservice-persist",
+							Type:                 "cli-persistent",
+							DBaaSEnvironment:     "production",
+							PersistentVolumePath: "/storage/data",
+							PersistentVolumeName: "nginx-php",
+							NativeCronjobs: []lagoon.Cronjob{
+								{
+									Name:     "cronjob-myservice-my-cronjobbb",
+									Service:  "myservice",
+									Command:  "sleep 300",
+									Schedule: "5 2 * * *",
+								},
+							},
+						},
+					},
+				},
+			},
+			want: "test-resources/cronjob/result-cli-2.yaml",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
