b09e30642abb91645635948884721af74e7ceec4
shreddedbacon <b@benjackson.email>
2025-06-10 12:18:54 +1000

    feat: add external service type

---
+++ internal/servicetypes/external.go
@@ -0,0 +1,7 @@
+package servicetypes
+
+// defines an external service type
+// this type doesn't have any associated data so this just acts as a placeholder
+var external = ServiceType{
+	Name: "external",
+}
